a8d0a15fc513ba84422f6ca36e38d9f3aeb59652
Monika Sikora <monikastanko90@gmail.com>
2024-06-11 20:21:59 +0200

    Navigation added with scrolling

--- psie-milo/src/app/app.module.ts
+++ psie-milo/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './content/main/main.component';
 import { ServicesComponent } from './content/services/services.component';
 import { AboutComponent } from './content/about/about.component';
 import { ContactComponent } from './content/contact/contact.component';
 import { LocalizationComponent } from './content/localization/localization.component';
-import { FooterComponent } from './footer/footer.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
-const materials = [MatSidenavModule];
+const materials = [MatSidenavModule, MatButtonModule];
 
 @NgModule({
   declarations: [
--- psie-milo/src/app/header/header.component.ts
+++ psie-milo/src/app/header/header.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  scrollTo(landingId: string) {
+    const element = document.getElementById(landingId);
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
