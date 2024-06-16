1ea2b9bb6d4cf7588f66933396088fe427c659be
Monika Sikora <monikastanko90@gmail.com>
2024-06-16 20:30:37 +0200

    Dodana mapa, navigacja dla wersji mobilnej

--- psie-milo/src/app/app.module.ts
+++ psie-milo/src/app/app.module.ts
@@ -5,24 +5,31 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './content/main/main.component';
-import { ServicesComponent } from './content/services/services.component';
 import { AboutComponent } from './content/about/about.component';
+import { ServicesComponent } from './content/services/services.component';
+import { TeamComponent } from './content/team/team.component';
 import { ContactComponent } from './content/contact/contact.component';
 import { LocalizationComponent } from './content/localization/localization.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
-const materials = [MatSidenavModule, MatButtonModule];
+const materials = [
+  MatSidenavModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
-    ServicesComponent,
     AboutComponent,
+    ServicesComponent,
+    TeamComponent,
     ContactComponent,
     LocalizationComponent,
     FooterComponent,
